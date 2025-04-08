a891fb8da45f34fb8fcbc6df4c85936a7088d31c
aasmah <aas.29980@gmail.com>
2025-04-07 22:59:30 -0500

    Update mapanimation -trip.js

--- mapanimation -trip.js
+++ mapanimation -trip.js
@@ -2,34 +2,18 @@
 const canyons = [
     [-114.442989, 36.540734],
     [-114.532962, 36.485874],
-
-
     [-112.9884495, 37.200947],
-
-
-
     [-112.528548, 37.048142],
-
     [-112.153309, 37.712820],
-
-
     [-112.167047, 37.662321],
-
-
-
-
     [-112.140631, 36.054515],
-
     [-114.721977, 36.141171],
-
     [-115.163235, 36.100812],
-
-
-
 ];
 
 // TODO: add your own access token
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWFzbWFoODAiLCJhIjoiY2xlcmRwa3c0MGEyNDN0cXRqZGhzd3R2eSJ9.UJ-Z1_jLdagx-AD4wKMqeg';
+mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN'; // Replace with your actual access token
+
 // This is the map instance
 let map = new mapboxgl.Map({
     container: 'map',
@@ -37,26 +21,30 @@ let map = new mapboxgl.Map({
     center: [-112.167047, 37.662321],
     zoom: 5,
 });
-// TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
+
+// TODO: add a marker to the map at the first coordinates in the array canyons. The marker variable should be named "marker"
 // Create a new marker.
-var marker = new mapboxgl.Marker()
-    //sample code marker.setLngLat([-114.532962,36.485874]);
-    //use upperCase / camelCase for setLngLat
-marker.setLngLat(canyons[0]);
-marker.addTo(map);
+let marker = new mapboxgl.Marker()
+    // Corrected: Use camelCase for setLngLat and access the first element of the canyons array
+    .setLngLat(canyons[0])
+    .addTo(map);
 
+// counter here represents the index of the current canyon
+let counter = 0;
 
-// counter here represents the index of the current bus stop
-var counter = 0;
-//sample code  move frm dr Abel's lesson
 function move() {
     // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
     setTimeout(() => {
-        // Use counter to access bus stops in the array busStops
-        if (counter >= canyons.length) return;
+        // Use counter to access canyons in the array canyons
+        if (counter >= canyons.length) return;  // Stop if counter exceeds the array length
+
+        // Corrected: Use camelCase for setLngLat and access the current canyon using the counter
         marker.setLngLat(canyons[counter]);
         counter++;
         // Make sure you call move() after you increment the counter.
-        move();
+        move(); // Corrected: Recursively call move to continue the animation
     }, 1000);
 }
+
+// Start the animation.
+move();
